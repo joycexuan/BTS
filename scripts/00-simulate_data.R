fa8e73f780eec2a3fabd2a3dc4f31a672781a532
joycexuan <joyceruoxuan@gmail.com>
2023-04-18 20:05:08 -0700

    simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,23 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates the album name, average danceability, and album release year of BTS dance dataset
+# Author: Joyce Xuan
+# Data: 20 April 2023
+# Contact: joyceruoxuan@gmail.com
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Spotify API Account
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+album_name <- dance$album_name
+set.seed(44)
 
-
-
+#Simulating three columns from the dance dataset
+bts_avg_data <- tibble(
+  album_name = c(sample(album_name, 100, TRUE, NULL)),
+  avg_dance = c(runif(100, 0, 1)),
+  album_release_year = c(sample(2013:2022, 100, TRUE))
+)
