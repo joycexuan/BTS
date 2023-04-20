d79e17170d9e6de1429f1917fe8c2e8b0143388a
joycexuan <joyceruoxuan@gmail.com>
2023-04-20 01:12:47 -0700

    fixed tests

--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -2,23 +2,28 @@
 # Purpose: Tests the data from BTS_mean dataset
 # Author: Joyce Xuan
 # Date: 20 April 2023
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Contact: joyceruoxuan@gmail.com [
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 
 #### Workspace setup ####
 library(tidyverse)
 
+#Reading in the data
+
 BTS <- read_csv(here::here("inputs/data/BTS_mean.csv"), show_col_types = FALSE)
 
+
+#Checking earliest releast year
 BTS$album_release_year |> 
   min() == 2013
 
+#Checking last release year
 BTS$album_release_year |> 
   max() == 2022
 
+#Checking that the album names are alphabetical characters
 BTS$album_name |>
   class() == "character"
 
